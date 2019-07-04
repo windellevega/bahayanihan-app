b00929cd3220681d4dde548d1c3a40f75815a261
Windelle John Vega <windellevega@gmail.com>
2019-07-04 17:43:46 +0800

    Map gets current location of device when being loaded.

--- src/app/components/map-tab/map-tab.page.ts
+++ src/app/components/map-tab/map-tab.page.ts
@@ -1,5 +1,22 @@
 import { Component, OnInit, ViewChild, ElementRef} from '@angular/core';
-import { Map, latLng, tileLayer, Layer, marker } from 'leaflet';
+import leaflet from 'leaflet';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+
+import { icon, Marker } from 'leaflet';
+const iconRetinaUrl = 'assets/marker-icon-2x.png';
+const iconUrl = 'assets/marker-icon.png';
+const shadowUrl = 'assets/marker-shadow.png';
+const iconDefault = icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
+});
+Marker.prototype.options.icon = iconDefault;
 
 @Component({
   selector: 'app-map-tab',
@@ -7,21 +24,41 @@ import { Map, latLng, tileLayer, Layer, marker } from 'leaflet';
   styleUrls: ['./map-tab.page.scss'],
 })
 export class MapTabPage implements OnInit {
-  map: Map;
+  map: any;
+  lat: any;
+  long: any;
+  constructor(public geo: Geolocation) {
 
-  ionViewDidEnter() {
-    this.loadLeafletMap();
   }
 
   ngOnInit() {
+    
+  }
 
+  ionViewWillEnter() {
+    this.loadLeafletMap();
   }
 
   loadLeafletMap() {
-    this.map = new Map('map').setView([17.618955, 121.724668], 17);
-    tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      zoomControl: true
-    }).addTo(this.map);
+      this.geo.getCurrentPosition().then(pos => {
+        this.lat = pos.coords.latitude;
+        this.long = pos.coords.longitude;
+
+        if(this.map !== undefined) {
+          this.map.off();
+          this.map.remove();
+        }
+
+        this.map = leaflet.map('map').setView([this.lat, this.long], 16); 
+
+        leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+          zoomControl: true
+        })
+        .addTo(this.map);
+
+        leaflet.marker([this.lat, this.long]).addTo(this.map);
+      })
+      .catch(err => console.log(err));
   }
 }
