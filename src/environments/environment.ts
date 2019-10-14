1763e7fa0d3b10ee76b7f38e0d3056d4788e44f6
Windelle John Vega <windellevega@gmail.com>
2019-10-14 17:07:10 +0800

    NPM update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,10 +5,15 @@
 export const environment = {
   production: false,
   //apiUrl: 'https://evening-mesa-93885.herokuapp.com',
-  apiUrl: 'http://localhost:8000',
+  apiUrl: 'http://ec2-3-17-5-56.us-east-2.compute.amazonaws.com',
+  //apiUrl: 'http://localhost:8000',
+  // oauth: {
+  //   clientId: '2',
+  //   clientSecret: 'kwuNG08lA3y9zhQFD8rgytWvL7GJyij1DASJ25FR'
+  // }
   oauth: {
     clientId: '2',
-    clientSecret: 'kwuNG08lA3y9zhQFD8rgytWvL7GJyij1DASJ25FR'
+    clientSecret: 'DXlqeXqoDpEN50v17s9qO4TaQUqRb46kaTGqnx0r'
   }
 };
 
