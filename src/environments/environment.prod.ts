034296980390be8806421ca5ae24f389ef8b20c4
Windelle John Vega <windellevega@gmail.com>
2019-10-15 14:53:44 +0800

    Updates on environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://evening-mesa-93885.herokuapp.com',
+  apiUrl: 'http://ec2-3-17-5-56.us-east-2.compute.amazonaws.com',
   //apiUrl: 'http://localhost:8000',
   oauth: {
     clientId: '2',
-    clientSecret: 'kwuNG08lA3y9zhQFD8rgytWvL7GJyij1DASJ25FR'
+    clientSecret: 'DXlqeXqoDpEN50v17s9qO4TaQUqRb46kaTGqnx0r'
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,16 +5,16 @@
 export const environment = {
   production: false,
   //apiUrl: 'https://evening-mesa-93885.herokuapp.com',
-  apiUrl: 'http://ec2-3-17-5-56.us-east-2.compute.amazonaws.com',
-  //apiUrl: 'http://localhost:8000',
-  // oauth: {
-  //   clientId: '2',
-  //   clientSecret: 'kwuNG08lA3y9zhQFD8rgytWvL7GJyij1DASJ25FR'
-  // }
+  //apiUrl: 'http://ec2-3-17-5-56.us-east-2.compute.amazonaws.com',
+  apiUrl: 'http://localhost:8000',
   oauth: {
     clientId: '2',
-    clientSecret: 'DXlqeXqoDpEN50v17s9qO4TaQUqRb46kaTGqnx0r'
+    clientSecret: 'kwuNG08lA3y9zhQFD8rgytWvL7GJyij1DASJ25FR'
   }
+  // oauth: {
+  //   clientId: '2',
+  //   clientSecret: 'DXlqeXqoDpEN50v17s9qO4TaQUqRb46kaTGqnx0r'
+  // }
 };
 
 /*
