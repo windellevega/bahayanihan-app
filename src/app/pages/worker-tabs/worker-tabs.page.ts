ff3ecfe51add22adabfc33ef7427d61481cdb0c7
Windelle John Vega <windellevega@gmail.com>
2020-03-09 21:25:11 +0800

    Added updating of user location on worker-tabs page

--- src/app/pages/worker-tabs/worker-tabs.page.ts
+++ src/app/pages/worker-tabs/worker-tabs.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-worker-tabs',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkerTabsPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+    private geo: Geolocation) { }
 
   ngOnInit() {
+    this.updateUserLocation();
   }
 
+  updateUserLocation() {
+    this.geo.getCurrentPosition().then(pos => {
+      this.userService.updateUserLocation(pos.coords.latitude, pos.coords.longitude).subscribe();
+    })
+    .catch(err => {
+      alert('Your location is not enabled');
+    });
+  }
 }
